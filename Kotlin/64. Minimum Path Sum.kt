c967b70336c53f03203590127d3c281ccf1dba91
wsi1212 <wsi1212@dgsw.hs.kr>
2023-08-16 20:02:36 +0900

    P 64
    8/16

---
+++ Kotlin/64. Minimum Path Sum.kt
@@ -0,0 +1,23 @@
+import kotlin.math.min
+fun minPathSum(grid: Array<IntArray>): Int {
+    for (i:Int in 0 until grid.size) {
+        for (j:Int in 0 until grid[0].size) {
+            if (i != 0 && j != 0)
+                grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
+            else if (j == 0 && i != 0) {
+                grid[i][j] += grid[i - 1][j]
+            } else if (i == 0 && j != 0) {
+                grid[i][j] += grid[i][j - 1]
+            }
+        }
+    }
+    return grid[grid.size - 1][grid[0].size - 1]
+}
+
+fun main() {
+    var test = Array(3) { IntArray(3) }
+    test = arrayOf(intArrayOf(1,3,1),
+        intArrayOf(1,5,1),
+        intArrayOf(4,2,1))
+    print(minPathSum(test))
+}
