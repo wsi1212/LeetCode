ea30498d74732ec530a4aa274fe62c707c7604fe
wsi1212 <wsi1212@dgsw.hs.kr>
2023-09-11 20:14:28 +0900

    Add: 931. Minimum Falling Path Sum.kt
    9/11

---
+++ Kotlin/DynamicProgramming/931. Minimum Falling Path Sum.kt
@@ -0,0 +1,18 @@
+package DynamicProgramming
+
+fun minFallingPathSum(matrix: Array<IntArray>): Int {
+    for (i in 1 until matrix.size) {
+        for (j in matrix.indices) {
+            matrix[i][j] += minOf(if (j > 0) matrix[i - 1][j - 1] else Int.MAX_VALUE, matrix[i - 1][j],if (j != matrix.size - 1) matrix[i - 1][j + 1] else Int.MAX_VALUE)
+        }
+    }
+    return matrix.last().min()
+}
+
+fun main() {
+    val test = arrayOf(
+        intArrayOf(2, 1, 3),
+        intArrayOf(6, 5, 4),
+        intArrayOf(7, 8, 9))
+    println(minFallingPathSum(test))
+}
